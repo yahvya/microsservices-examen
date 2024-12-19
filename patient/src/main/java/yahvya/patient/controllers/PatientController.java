d76a4b181768f3d28858f4bf037a9ef3cfec31ae
Yahaya <developpeur.yb@gmail.com>
2024-12-19 11:42:50 +0100

    add patient modify, list , and get particular patient route

--- patient/src/main/java/yahvya/patient/controllers/PatientController.java
+++ patient/src/main/java/yahvya/patient/controllers/PatientController.java
@@ -2,7 +2,6 @@ package yahvya.patient.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import yahvya.patient.datacontracts.patient.Patient;
 import yahvya.patient.datacontracts.patient.PatientService;
@@ -27,5 +26,46 @@ public class PatientController {
         Patient createdPatient = this.patientService.addPatient(patient);
 
         return new Response(createdPatient != null,createdPatient);
+    }
+
+    /**
+     * @brief modify a patient
+     * @param patient form patient
+     * @return response
+     */
+    @PostMapping(name="patient.modify",path="/modify")
+    public Response modifyPatient(@RequestBody @Valid Patient patient) {
+        Patient modifiedPatient = this.patientService.modifyPatient(patient);
+
+        return new Response(modifiedPatient != null,modifiedPatient);
+    }
+
+    /**
+     * @brief patients list
+     * @return list
+     */
+    @GetMapping(name="patient.list",path = "/list")
+    public Response listPatient() {
+        return new Response(true,this.patientService.getAll());
+    }
+
+    /**
+     * @brief get particular patient info
+     * @param patientId patient id
+     * @return patient info
+     */
+    @GetMapping(name="patient.info",path = "/info/{patientId}")
+    public Response getInfo(@PathVariable String patientId) {
+        return new Response(true,this.patientService.getPatient(patientId));
+    }
+
+    /**
+     * @brief delete particular patient info
+     * @param patientId patient id
+     * @return patient info
+     */
+    @PostMapping(name="patient.delete",path = "/delete/{patientId}")
+    public Response delete(@PathVariable String patientId) {
+        return new Response(this.patientService.deletePatient(patientId),null);
     }
 }
--- patient/src/main/java/yahvya/patient/datacontracts/patient/PatientService.java
+++ patient/src/main/java/yahvya/patient/datacontracts/patient/PatientService.java
@@ -3,6 +3,8 @@ package yahvya.patient.datacontracts.patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * @brief patient interactions service
  */
@@ -25,6 +27,51 @@ public class PatientService {
         }
         catch(Exception e){
             return null;
+        }
+    }
+
+    /**
+     * @brief modify the patient in database
+     * @param patient the patient to create
+     * @return the created patient data or null on failure
+     */
+    public Patient modifyPatient(Patient patient) {
+        try{
+            return this.patientRepo.save(patient);
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+
+    /**
+     * @return all patient
+     */
+    public List<Patient> getAll() {
+        return this.patientRepo.findAll();
+    }
+
+    /**
+     * @brief get a patient data by id
+     * @param id id
+     * @return patient data
+     */
+    public Patient getPatient(String id) {
+        return this.patientRepo.findById(id).orElse(null);
+    }
+
+    /**
+     * @brief delete a patient data by id
+     * @param id id
+     * @return patient data
+     */
+    public boolean deletePatient(String id) {
+        try{
+            this.patientRepo.deleteById(id);
+            return true;
+        }
+        catch(Exception e){
+            return false;
         }
     }
 }
